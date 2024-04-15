54b1c74765ac22bda810e5a903b936f7517de711
Boyun Lee <127330598+JumpingDragon@users.noreply.github.com>
2024-04-15 15:39:03 +0900

    Update dbtest01.py

--- dbtest01.py
+++ dbtest01.py
@@ -5,7 +5,7 @@ import pymysql  # mysql과 연동 시켜주는 라이브러리
 # 2) 비밀번호 : 12345
 # 3) 데이터베이스가 설치된 컴퓨터의 IP 주소
 #   - 본인 컴퓨터면 localhost, 다른 컴퓨터면 그 컴퓨터의 ip 주소
-#   - 192.168.0.177 (이 컴퓨터 ip 주소)
+#   - 192.168.0.??? (컴퓨터 ip 주소)
 # 4) 데이터베이스 스키마 이름 (ex: shopdb)
 
 dbConn = pymysql.connect(host='localhost',user='root', password='12345', db='shopdb')
